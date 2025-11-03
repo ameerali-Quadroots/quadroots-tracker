f5a0145abde68b1bd4cb236c68c97c27c72f726a
Ameer Ali <ameer@quadroots.com>
2025-11-04 01:59:15 +0500

    layout change for employees

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,35 @@
 module ApplicationHelper
+  def edit_requests_count
+    if current_user.role == "Manager" && current_user.department != "HOD'S"
+      EditRequest.where(department: current_user.department)
+                 .where.not(user_id: current_user.id)
+                 .count
+    elsif current_user.department == "HOD'S"
+      departments = ["WEB", "SEO", "ADS", "CONTENT"]
+      manager_ids = User.where(role: "Manager", department: departments).pluck(:id)
+      EditRequest.where(user_id: manager_ids).count
+    else
+      0
+    end
+  end
+
+   def leaves_count
+    if current_user.role == "Manager" && current_user.department != "HOD'S"
+      # Regular Manager: sees Executives in their department
+      Leave.joins(:user)
+           .where(users: { role: "Executive", department: current_user.department })
+           .count
+
+    elsif current_user.role == "Manager" && current_user.department == "HOD'S"
+      # HOD Manager: sees Managers from specific departments
+      hod_departments = ["WEB", "SEO", "ADS", "CONTENT"]
+
+      Leave.joins(:user)
+           .where(users: { role: "Manager", department: hod_departments })
+           .count
+
+    else
+      0
+    end
+    end
 end
--- app/models/break.rb
+++ app/models/break.rb
@@ -1,6 +1,6 @@
 class Break < ApplicationRecord
   belongs_to :time_clock
-  VALID_BREAK_TYPES = ["Meal Break - 40 Minutes ","Second Break - 30 minutes", "Meeting","Extra Break", "Downtime"]
+  VALID_BREAK_TYPES = ["Meal Break - 40 Minutes","Second Break - 30 minutes", "Meeting","Extra Break", "Downtime"]
 
   validates :break_type, inclusion: { in: VALID_BREAK_TYPES }
 
